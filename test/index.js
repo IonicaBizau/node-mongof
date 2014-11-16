2b44d0ad700a9fc7bb2bb277fa2f03f8f33e5eba
Ionică Bizău <bizauionica@yahoo.com>
2014-11-16 19:31:23 +0200

    Use `collections` array for syncing databases.

--- test/index.js
+++ test/index.js
@@ -3,9 +3,6 @@ var MongoSyncFiles = require("../index")
   , Faker = require("faker")
   ;
 
-// Create database instance
-var MyDatabase = new MongoSyncFiles();
-
 function generateFakeDataArray() {
     var docs = [];
     for (var i = 0; i < 30; ++i) {
@@ -18,17 +15,21 @@ function generateFakeDataArray() {
     return docs;
 }
 
-// Create collection instance
-var MyAwesomeCollection = MyDatabase.initCollection({
-    inputFile: __dirname + "/docs-in.json"
-  , outputFile: __dirname + "/docs-out.json"
-  , uri: "mongodb://localhost:27017/test"
-  , collection: "myCol"
-  , autoInit: true
-}, function (err) {
+// Create database instance
+var MyDatabase = new MongoSyncFiles({
+    collections: [{
+        inputFile: __dirname + "/docs-in.json"
+      , outputFile: __dirname + "/docs-out.json"
+      , uri: "mongodb://localhost:27017/test"
+      , collection: "myCol"
+      , autoInit: true
+    }]
+}, function (err, collections) {
 
     // Handle error
     if (err) { throw err; }
+
+    var MyAwesomeCollection = collections.myCol;
 
     // Run a Mongo request
     MyAwesomeCollection.find({}).toArray(function (err, docs) {
